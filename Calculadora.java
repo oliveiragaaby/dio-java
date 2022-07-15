8a2205812b89eadefb8f0b32cfc05a848c00f2fc
Gabriella Oliveira <102620199+oliveiragaaby@users.noreply.github.com>
2022-07-15 14:19:37 -0300

    calc1

---
+++ Calculadora.java
@@ -0,0 +1,13 @@
+package dio;
+public class Calculadora {
+
+    public static void soma(double numero1,double numero2) {
+            double resultado = numero1 + numero2;
+            System.out.println(resultado);
+        }
+
+    public static void subtracao(double numero1, double numero2){
+            double resultado = numero1 - numero2;
+            System.out.println(resultado);
+        }
+}
