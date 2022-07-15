563044e741d72687b28fe724b9d29fd7001c1838
Gabriella Oliveira <102620199+oliveiragaaby@users.noreply.github.com>
2022-07-15 14:20:05 -0300

    mainFirst

---
+++ Main.java
@@ -0,0 +1,18 @@
+package dio;
+
+public class Main {
+    public static void main(String[] args) {
+        //Calculadora
+        Calculadora.soma(5, 5);
+        Calculadora.subtracao(5, 4);
+
+        //Horário
+        Mensagem.obterHora(5);
+
+        //Quadrilátero
+        Quadrilatero.area(5);
+        Quadrilatero.area(4, 2);
+        Quadrilatero.area(3, 5, 7);
+
+    }
+}
